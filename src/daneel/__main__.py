ce046ab2aa73991ec1bb97134402d39b28744bb7
mahdisrenia-coder <mahdis.rezaeiniasar@gmail.com>
2025-11-06 19:13:39 +0000

    added understanding codes function as comment and adding -t  argument and reading of it as transit + change of dest of atmosphere.

--- src/daneel/__main__.py
+++ src/daneel/__main__.py
@@ -1,12 +1,22 @@
+##mahdis' note: this is the main file that runs the program. It uses argparse to get input from the command line.
+# by command line () daneel -i path_to_parameters.yaml -t we meant:
+# hey daneel program which got run in my terminal: please take the input (-i) from the path_to_parameters.yaml
+# and do the transit (-t) function.
+
 import datetime
 import argparse
 from daneel.parameters import Parameters
 from daneel.detection import *
+from daneel import transit
+
+##mahdis' note: 
+# datetime lets us track when the program starts and ends.
+# now how this command line would work: we use argparse to define the arguments we want to pass.
 
 
 def main():
     parser = argparse.ArgumentParser()
-
+    ## mahdis' note: parser means
     parser.add_argument(
         "-i",
         "--input",
@@ -28,9 +38,18 @@ def main():
     parser.add_argument(
         "-a",
         "--atmosphere",
-        dest="complete",
+        dest="atmosphere",
         required=False,
         help="Atmospheric Characterisazion from input transmission spectrum",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--transit",
+        dest="transit",
+        required=False,
+        help="Plot transit light curve from input parameters",
         action="store_true",
     )
 
@@ -46,10 +65,11 @@ def main():
         pass
     if args.atmosphere:
         pass
+    if args.transit:
+        transit.transit(input_pars)
 
     finish = datetime.datetime.now()
     print(f"Daneel finishes at {finish}")
-
 
 if __name__ == "__main__":
     main()
