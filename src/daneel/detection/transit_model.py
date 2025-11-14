f402c0ecc27ba0e3ad926c40a7180b23879952f1
lcballard <lewballard@gmail.com>
2025-11-14 08:37:36 +0000

    transit_model.py accepting tiziano version

--- src/daneel/detection/transit_model.py
+++ src/daneel/detection/transit_model.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-##added this in class 31/10
-
-import matplotlib.pyplot as plt
-import batman
-import numpy as np
-
-class Transit_Model:
-    def __init__(self):
-
-        self.params = batman.TransitParams()
-        self.params.t0   = 0.                #time of inferior conjunction
-        self.params.per  = 433.0             #orbital period in days
-        self.params.rp   = 0.0213            #planet radius (in units of stellar radii)
-        self.params.a    = 222.343           #semi-major axis (in units of stellar radii)
-        self.params.inc  = 0.                #orbital inclination (in degrees)
-        self.params.ecc  = 0.08              #eccentricity
-        self.params.w    = -66               #longitude of periastron (in degrees)
-        self.params.u    = [u1, u2]          #limb darkening coefficients [u1, u2]
-        self.params.limb_dark = "quadratic"  #limb darkening model
-
-        self.t = np.linspace(-0.075, 0.075, 1000)
-
-        self.model = batman.TransitModel(self.params, self.t)
-
-    def compute_light_curve(self):
-
-        self.flux = self.model.light_curve(self.params)
-        return self.flux
-    
-    def plot_light_curve(self, output_file):
-
-        if not hasattr(self, 'flux'):
-            self.compute_light_curve()
-
-=======
 import numpy as np
 import batman
 import matplotlib.pyplot as plt
@@ -98,15 +62,11 @@ class TransitModel:
             self.compute_light_curve()
         
         plt.figure(figsize=(10, 6))
->>>>>>> 4ef3ba24b4b96603c344bbfd5d39e1f4a88e5b3d
         plt.plot(self.t, self.flux)
         plt.xlabel("Time from central transit (days)")
         plt.ylabel("Relative flux")
         plt.savefig(output_file)
         plt.show()
-<<<<<<< HEAD
-
-=======
         print(f"Light curve saved to {output_file}")
     
     def run(self, output_file='lc.png'):
@@ -120,4 +80,3 @@ class TransitModel:
         """
         self.compute_light_curve()
         self.plot_light_curve(output_file)
->>>>>>> 4ef3ba24b4b96603c344bbfd5d39e1f4a88e5b3d
